76372e773a291e18e22f8e8497b88518e293842f
Moises Valverde <moisesvg44@hotmail.com>
2020-03-30 23:04:46 -0600

    Vista ViewCitas agregada

---
+++ PetsCR/PetsCR/PetsCR/Vistas/ViewCitas.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PetsCR.Vistas
+{
+    public partial class ViewCitas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e) //GridDetallesCita
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e) //TbFecha
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e) //TbHora
+        {
+
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e) //TbIdentificacion
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) //BotonAgregar
+        {
+
+        }
+    }
+}
